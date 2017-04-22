828d221d1b5ce51efd048ea34ab9fcc0e96d6b7a
Cosmosquark <bthompson2090@gmail.com>
2017-04-22 16:55:34 +0100

    adding in scripts, commented out my bugs, catching the train back home

---
+++ Assets/Scripts/GameResources.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class resources : MonoBehaviour {
+
+	// Define the resource variables
+	private float nextUpdateTime = 0.0f;
+	public float updatePeriod = 0.1f;
+	public float Oxygen = 0.0f;
+	public float Biomass = 0.0f;
+	public float Iron = 0.0f;
+	public float Silicon = 0.0f;
+	public float Energy = 0.0f;
+
+    // Define the maximum resources required to win
+	public float MaxOxygen = 10.0f;
+	public float MaxBiomass = 10.0f;
+	public float MaxIron = 10.0f;
+	public float MaxSilicon = 10.0f;
+	public float MaxEnergy = 10.0f;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > nextUpdateTime) {
+			nextUpdateTime += updatePeriod;
+			Oxygen += 0.1f;
+			Biomass += 0.1f;
+			Iron += 0.1f;
+			Silicon += 0.1f;
+			Energy += 0.1f;
+			//Debug.Log (Oxygen);
+		}
+	}
+}
---
+++ Assets/UpdateResource.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateResource : MonoBehaviour {
+
+//	private GameObject game_mech = GameObject.Find("GameMechanics");
+	private GameObject game_mech;	//	private GameObject GUIText = gameObject.name;
+	// Use this for initialization
+
+	void Start () {
+	//	game_mech = GameObject.FindObjectOfType(typeof(
+	}
+
+	// Update is called once per frame
+	void Update () {
+/*		if (GUIText.name == "OxygenText") {
+			GUIText.t = Mathf.RoundToInt(game_mech.GetComponent<resources> ().Oxygen);
+		} 
+		if (GUItext.GetComponentInParent<name> () == "EnergyText") 
+		{
+			text = Mathf.RoundToInt(game_mech.GetComponent<resources> ().Energy);
+		} 
+		if ( GUItext.GetComponentInParent<name> () == "BiomassText")
+		{
+			text = Mathf.RoundToInt(game_mech.GetComponent<resources> ().Biomass);
+		}
+		if ( GUItext.GetComponentInParent<name> () == "SiliconText")
+		{
+			text = Mathf.RoundToInt(game_mech.GetComponent<resources> ().Silicon);
+		}
+		if ( GUItext.GetComponentInParent<name> () == "IronText")
+		{
+			text = Mathf.RoundToInt(game_mech.GetComponent<resources> ().Iron);
+		} */
+	}
+}
