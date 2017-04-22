2d5b3f17243ec3ab5685e6469e82944c951aac68
Cosmosquark <bthompson2090@gmail.com>
2017-04-22 15:05:32 +0100

    added the compute_area function to compute the overlapping area of two spherical caps

---
+++ Assets/Scripts/compute_area.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class compute_area : MonoBehaviour {
+	/// <summary>
+	/// Approximate the area of intersection of two sphrical caps
+	/// fRadus0 : First caps' radius (arc length in radians)
+	/// fRadius1 : Second caps' radius (in radians)
+	/// fDist : Distance between caps (radians between centres of the caps)
+	/// 
+	/// References:
+	///  -  Code is adapted from slide 15 in here http://developer.amd.com/wordpress/media/2012/10/Oat-AmbientApetureLighting.pdf
+	///     This is an effective approximation for the area.. the actual method is much more numerically intensive.
+	///  -  Spherical Cap https://en.wikipedia.org/wiki/Spherical_cap
+	///  -  Saturate: https://unitygem.wordpress.com/shader-part-1/ and http://http.developer.nvidia.com/Cg/saturate.html
+	///  -  SmoothStep: https://docs.unity3d.com/ScriptReference/Mathf.SmoothStep.html
+	/// 
+	/// </summary> 
+
+	public float fRadius0 = 0.0;
+	public float fRadius1 = 0.0;
+	public float fDist = 0.0;
+	public float fDiff = 0.0;
+	public float fArea = 0.0;
+	public float fSat = 0.0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void Saturate(){
+		fSat = Mathf.Max (0, 
+			Mathf.Min (1.0,
+				((fDist - fDiff) / (fRadius0 + fRadius1 - fDiff))
+			)
+		);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (fDist <= Mathf.Max (fRadius0, fRadius1) - Mathf.Min (fRadius0, fRadius1)) {
+			// one cap is completely inside the other
+			fArea = 6.283185308 - (6.283185308 * Mathf.Cos (Mathf.Min (fRadius0, fRadius1)));
+
+		} else if (fDist >= fRadius0 + fRadius1) {
+			// no intersection exists
+			fArea = 0;
+
+		} else {
+			// here we have an overlap
+			fDiff = Mathf.Abs(fRadius0 - fRadius1);
+			fSat = Saturate();
+			fArea = Mathf.SmoothStep (0.0, 1.0, fSat);
+			fArea *= 6.283185308 - (6.283185308 * Mathf.Cos (Mathf.Min (fRadius0, fRadius1)));
+		}
+
+	}
+}
