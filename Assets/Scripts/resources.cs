74bbab60e3ed3fd4838e6b36139d43ab33a96a9a
Cosmosquark <bthompson2090@gmail.com>
2017-04-22 15:39:22 +0100

    adding in the meta files

---
+++ Assets/Scripts/resources.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class resources : MonoBehaviour {
+
+	// Use this for initialization
+	public float Oxygen = 0.0f;
+	public float Biomass = 0.0f;
+	public float Iron = 0.0f;
+	public float Silicon = 0.0f;
+	public float Energy = 0.0f;
+	public float Bureaucracy = 0.0f;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
