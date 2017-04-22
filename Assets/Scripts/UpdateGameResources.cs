9e2eef85ef38db4bf71668a6f2c7c004bebae3c4
Cosmosquark <bthompson2090@gmail.com>
2017-04-22 23:22:50 +0100

    updating

--- Assets/Scripts/UpdateGameResources.cs
+++ Assets/Scripts/UpdateGameResources.cs
@@ -1,23 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UpdateResource : MonoBehaviour {
 
 //	private GameObject game_mech = GameObject.Find("GameMechanics");
 	private GameObject game_mech;	//	private GameObject GUIText = gameObject.name;
 	// Use this for initialization
+	private Text resource_text;
 
 	void Start () {
-	//	game_mech = GameObject.FindObjectOfType(typeof(
+		// game_mech = GameObject.FindObjectOfType (typeof(GameMechanics));
+		game_mech = GameObject.Find("GameMechanics");
+		resource_text = gameObject.GetComponent<Text> ();
 	}
 
 	// Update is called once per frame
 	void Update () {
-/*		if (GUIText.name == "OxygenText") {
-			GUIText.t = Mathf.RoundToInt(game_mech.GetComponent<resources> ().Oxygen);
+		if (gameObject.transform.parent.name == "OxygenText") {
+			resource_text.text = Mathf.RoundToInt(game_mech.GetComponent<resources>().Oxygen).ToString();
 		} 
-		if (GUItext.GetComponentInParent<name> () == "EnergyText") 
+/*		if (GUItext.GetComponentInParent<name> () == "EnergyText") 
 		{
 			text = Mathf.RoundToInt(game_mech.GetComponent<resources> ().Energy);
 		} 
