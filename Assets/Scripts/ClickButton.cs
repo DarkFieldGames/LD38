5e6ebc5bb5fe113dea389480007ce731c667bafd
Cosmosquark <bthompson2090@gmail.com>
2017-04-23 13:51:54 +0100

    added the click button scriot

---
+++ Assets/Scripts/ClickButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems; // Required when using Event data.
+
+public class ClickButton : MonoBehaviour, IPointerDownHandler {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void OnPointerDown (PointerEventData eventData){
+		Debug.Log (this.gameObject.name + " Was Clicked.");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
