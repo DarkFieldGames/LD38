d9d7790d78c6201fa74ef76720fd8d69bd738717
Cosmosquark <bthompson2090@gmail.com>
2017-04-23 21:25:34 +0100

    clicking on a button for each building spawn will now spawn a building of that tag and colour etc

--- Assets/Scripts/ClickButton.cs
+++ Assets/Scripts/ClickButton.cs
@@ -6,14 +6,34 @@ using UnityEngine.EventSystems; // Required when using Event data.
 
 public class ClickButton : MonoBehaviour, IPointerDownHandler {
 
+	private GameObject game_mech;
+	private SelectedResource building_type;
 	// Use this for initialization
 	void Start () {
-		
+		game_mech = GameObject.FindGameObjectWithTag("GameMechanics");
+		SelectedResource building_type = game_mech.GetComponent<SelectedResource>();
 	}
 
 	public void OnPointerDown (PointerEventData eventData){
 		// TODO: prevent the placement of a building when this is clicked
+		SelectedResource building_type = game_mech.GetComponent<SelectedResource>();
 		Debug.Log (this.gameObject.name + " Was Clicked.");
+		if (this.gameObject.name == "oxygenButton") {
+			building_type.resource = "Oxygen";
+		}
+		if (this.gameObject.name == "ironButton") {
+			building_type.resource = "Iron";
+		}
+		if (this.gameObject.name == "energyButton") {
+			building_type.resource = "Energy";
+		}
+		if (this.gameObject.name == "biomassButton") {
+			building_type.resource = "Biomass";
+		}
+		if (this.gameObject.name == "siliconButton") {
+			building_type.resource = "Silicon";
+		}
+
 	}
 
 	// Update is called once per frame
--- Assets/Scripts/SphereSelect.cs
+++ Assets/Scripts/SphereSelect.cs
@@ -2,21 +2,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class SphereSelect : MonoBehaviour {
 
 	public GameObject CurrentPrefab;
 
 	public float RadiusRatio = 1.0f;
+	private Color mycolor;
+	private String tag_string;
 
 	// Use this for initialization
 	void Start () {
-		
+		Color myColor = Color.black;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
+		Color myColor = Color.black;
 		bool dont_generate = false;
 		if (!Input.GetMouseButtonDown(0))
 			return;
@@ -29,6 +31,33 @@ public class SphereSelect : MonoBehaviour {
 
 		if(Physics.Raycast(ray, out hit, maxDistance: 1000, layerMask: 1 << 8))
 		{
+			GameObject game_mech = GameObject.FindGameObjectWithTag("GameMechanics");
+			SelectedResource building_type = game_mech.GetComponent<SelectedResource>();
+			Debug.Log ("building type");
+			Debug.Log (building_type.resource);
+
+			// select the colour and the tag
+			if (building_type.resource == "Oxygen") {
+				myColor = Color.blue;
+				tag_string = "OxygenBuilding";
+			}
+			if (building_type.resource == "Iron") {
+				myColor = Color.red;
+				tag_string = "IronBuilding";
+			}
+			if (building_type.resource == "Silicon") {
+				myColor = Color.yellow;
+				tag_string = "SiliconBuilding";
+			}
+			if (building_type.resource == "Biomass") {
+				myColor = Color.green;
+				tag_string = "BiomassBuilding";
+			}
+			if (building_type.resource == "Energy") {
+				myColor = Color.magenta;
+				tag_string = "EnergyBuilding";
+			}
+
 			var newVector = hit.point * RadiusRatio;
 
 
@@ -50,6 +79,8 @@ public class SphereSelect : MonoBehaviour {
 			if (dont_generate == false) 
 			{
 			    var o = (GameObject) Instantiate(CurrentPrefab, newVector, Quaternion.FromToRotation(Vector3.up, hit.normal));
+				o.GetComponent<UpdateBuildingSize> ().BuildingColor = myColor;
+				o.tag = tag_string;
 			}
 			//o.transform.position = hit.point;
 			//o.transform.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
